6cae4bca7dc537f2a74ec7e1ec6c69065efe546a
Gabriel LE GALL <71171030+gabriellegall@users.noreply.github.com>
2020-11-15 13:21:02 +0100

    Add files via upload

---
+++ Law-of-large-numbers/law_of_large_numbers_animated_v1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+# parameters
+max_sample_size  = 500
+theoretical_mean = 0.5
+
+# dynamic plot
+sample_sizes, sample_averages = [0], [0]
+fig, ax = plt.subplots(1, 1, figsize = (16, 9))
+line, = ax.plot(sample_sizes, sample_averages)
+plt.axhline(y=theoretical_mean, color='r', linestyle='-')
+plt.legend(('sample average','theoretical average'))
+plt.ylim(0, 1)
+ax.set_title('Sample average per sample size')
+ax.set_xlabel('sample size')
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_color('lightgrey')
+ax.spines['left'].set_color('lightgrey')
+ax.tick_params(axis='x', colors='grey')
+ax.tick_params(axis='y', colors='grey')
+
+def animate(i):
+    sample_sizes.append(i)
+    sample_averages.append(np.random.randint(2, size=i).mean())
+    line.set_data(sample_sizes,sample_averages)
+    plt.xlim(min(sample_sizes), max(sample_sizes))
+
+anim = animation.FuncAnimation(fig,
+    animate,
+    max_sample_size,
+    interval = 1,
+    repeat=False)
+plt.show()
